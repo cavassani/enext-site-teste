3c91e196077c29ffdd265c81a7113b078d436ffd
Marcelo Cavassani <marceloscavassani@gmail.com>
2017-11-26 20:33:22 -0200

    ajustes de  footer e caldeirao do topo da pagina|inicio de js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,27 @@
 function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'assets/potions.json', true);
+  xobj.onreadystatechange = function() {
+    if (xobj.readyState == 4 && xobj.status == "200") {
 
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'assets/potions.json', true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
 
-            callback(xobj.responseText);
-
-        }
     }
-    xobj.send(null);
+  }
+  xobj.send(null);
 
 }
 
-
 loadJSON(function(response) {
-    jsonresponse = JSON.parse(response);
+  jsonresponse = JSON.parse(response);
 
-    console.log(jsonresponse.potions);
+  console.log(jsonresponse.potions);
+  potions = jsonresponse.potions;
+  return potions;
 
 });
+
+// function popData(potion_id) {
+//
+// }
